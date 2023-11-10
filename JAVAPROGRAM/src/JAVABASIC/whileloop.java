a3502e80b284dde5509cf48122e4e3a13515968f
Saliha <zawjatitausif@gmail.com>
2023-11-10 09:45:32 +0100

    10-11-2023

---
+++ JAVAPROGRAM/src/JAVABASIC/whileloop.java
@@ -0,0 +1,14 @@
+package JAVABASIC;
+
+public class whileloop {
+
+	public static void main(String[] args) {
+		int i= 1;
+		while (i<5)
+		{System.out.println("I am Saliha");
+		i++;
+		}
+
+	}
+
+}
